bffb44da48e122ceddb60f980cfe59be3d699620
Bond1568 <1063143017@qq.com>
2018-03-25 11:23:22 +0800

    add in local master

--- tinyTool/src/tinyTool/Test.java
+++ tinyTool/src/tinyTool/Test.java
@@ -3,6 +3,7 @@ package tinyTool;
 public class Test {
 	// add in ding's forked item
 	// add in bond's master
+	// add in bond's local master
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println("hello");
