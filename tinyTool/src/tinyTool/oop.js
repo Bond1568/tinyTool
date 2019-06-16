9d12c589fb2193cecc6096f74225c68858d9acd0
DeYueQiQi <wujiahao98@126.com>
2019-06-16 22:36:56 +0800

    Create oop.js

---
+++ tinyTool/src/tinyTool/oop.js
@@ -0,0 +1,36 @@
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
+
+Person.prototype.hi = function()  {
+	console.log('Hi, my name is' + this.name+','+this.age+'years old');
+}
+
+Person.prototype.LEGS_NUM = 2;
+Person.prototype.ARMS_NUM = 2;
+Person.prototype.walk = function() {
+	console.log(this.name+"is walking");
+};
+
+function Student(name, age, className) {
+	Person.call(this, name, age);
+	this.className = className;
+}
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+Student.prototype.hi = function() {
+	console.log('Hi, my name is' + this.name+','+this.age+'years old');
+};
+
+Student.prototype.learn = function(subject) {
+	console.log(this.name + 'is learning' +subject+'at'+this.className+'.');
+};
+
+var bosn = new Student('Bosn','27','Class 3');
+bosn.hi();
+bosn.LEGS_NUM;
+bosn.walk();
+bosn.learn('math');
